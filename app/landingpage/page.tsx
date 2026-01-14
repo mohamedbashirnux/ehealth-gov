ed4b0d7e88dbf55a5f6c2680b59419dc786bee7b
Mohamed Bashir <mohamedbashirnux@gmail.com>
2026-01-14 14:09:41 +0300

    Remove scroll indicator animation from landing page

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -334,13 +334,6 @@ export default function LandingPage() {
             </Link>
           </div>
         </div>
-
-        {/* Scroll Indicator */}
-        <a href="#services" className="absolute bottom-8 left-1/2 transform -translate-x-1/2 cursor-pointer">
-          <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center animate-bounce">
-            <div className="w-1 h-3 bg-white rounded-full mt-2"></div>
-          </div>
-        </a>
       </section>
 
       {/* Features Section */}
